681a331e66e4a420f426fe197e14a6e753c2901e
Josteinas <josteinsolaas@gmail.com>
2014-08-13 02:12:48 +0200

    login layout

--- src/com/lavalampe/appenings/LoginActivity.java
+++ src/com/lavalampe/appenings/LoginActivity.java
@@ -1,15 +1,28 @@
 package com.lavalampe.appenings;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.TextView;
+ 
 public class LoginActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setting default screen to login.xml
+        setContentView(R.layout.activity_login);
+ 
+        TextView registerScreen = (TextView) findViewById(R.id.link_to_register);
+ 
+        // Listening to register new account link
+        registerScreen.setOnClickListener(new View.OnClickListener() {
+ 
+            public void onClick(View v) {
+                // Switching to Register screen
+                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+    }
 }
