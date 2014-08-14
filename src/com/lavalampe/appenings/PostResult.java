1ed2a8d9ecb0c139db9af0a6d73e2b13806f6bd3
haakondi <hakon.dissen@gmail.com>
2014-08-14 19:08:33 +0200

    postresult

---
+++ src/com/lavalampe/appenings/PostResult.java
@@ -0,0 +1,16 @@
+package com.lavalampe.appenings;
+
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.cookie.Cookie;
+
+public class PostResult {
+	public final HttpResponse response;
+	public final List<Cookie> cookies;
+	public PostResult(HttpResponse response, List<Cookie> cookies) {
+		super();
+		this.response = response;
+		this.cookies = cookies;
+	}
+}
