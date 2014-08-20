1628351abec814f118f942333cc23e616c09777d
haakondi <hakon.dissen@gmail.com>
2014-08-20 12:36:11 +0200

    Added callbacks to UI thread for asyncpost

---
+++ src/com/lavalampe/appenings/EventListFragment.java
@@ -0,0 +1,27 @@
+package com.lavalampe.appenings;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EventListFragment extends ListFragment{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
+	
+}
---
+++ src/com/lavalampe/appenings/networking/AsyncPost.java
@@ -0,0 +1,75 @@
+package com.lavalampe.appenings.networking;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.lavalampe.appenings.LoginActivity;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class AsyncPost extends AsyncTask<NameValuePair, Integer, PostResult> {
+	private static final String TAG = AsyncPost.class.getSimpleName();
+
+	private HttpPost httpPost;
+	private AsyncPostCallback callback;
+
+	public AsyncPost(String serverUrl, AsyncPostCallback callback) {
+		super();
+		httpPost = new HttpPost(serverUrl);
+		this.callback = callback;
+	}
+
+	public AsyncPost(String serverUrl) {
+		super();
+		httpPost = new HttpPost(serverUrl);
+	}
+
+	@Override
+	protected PostResult doInBackground(NameValuePair... params) {
+
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+
+		try {
+			List<NameValuePair> nameValuePairs = Arrays.asList(params);
+			httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+			HttpResponse response = httpClient.execute(httpPost);
+			Log.d(LoginActivity.class.getSimpleName(), response.toString());
+
+			List<Cookie> cookies = httpClient.getCookieStore().getCookies();
+			PostResult result = new PostResult(response, cookies);
+			return result;
+		} catch (UnsupportedEncodingException e) {
+			Log.e(TAG, e.getMessage());
+		} catch (ClientProtocolException e) {
+			Log.e(TAG, e.getMessage());
+			e.printStackTrace();
+		} catch (IOException e) {
+			Log.e(TAG, e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	protected void onPostExecute(PostResult result) {
+		// TODO Auto-generated method stub
+		super.onPostExecute(result);
+		if (callback != null)
+			callback.onPostComplete(result);
+	}
+	
+	
+
+}
