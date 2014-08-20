55ae96eb65b8d6e234aa88e03ff4eadcf3b942d0
Josteinas <josteinsolaas@gmail.com>
2014-08-20 21:22:58 +0200

    custom list items

---
+++ src/com/lavalampe/appenings/CategoryListAdapter.java
@@ -0,0 +1,59 @@
+package com.lavalampe.appenings;
+
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CategoryListAdapter extends ArrayAdapter<Category> {
+
+	public CategoryListAdapter(Context context, int resource) {
+		super(context, resource);
+	}
+
+	public CategoryListAdapter(Context context, int resource,
+			List<Category> items) {
+		super(context, resource, items);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		
+		ViewHolder holder;
+		
+		if (convertView == null) {
+			LayoutInflater vi = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			convertView = vi.inflate(R.layout.category_list_item, parent);
+			
+			holder = new ViewHolder();
+			holder.header = (TextView) convertView.findViewById(R.id.category_item_header);
+			holder.map = (TextView) convertView.findViewById(R.id.category_item_maptemp);
+			holder.follow = (Button) convertView.findViewById(R.id.btnFollow);
+			
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+		
+		holder.header.setText(getItem(position).getName());
+		holder.map.setText("This will be a map");
+		holder.follow.setText("Following");
+		holder.follow.setSelected(true);
+		holder.follow.setBackgroundColor(Color.GREEN);
+		
+		return convertView;
+	}
+	
+	static class ViewHolder {
+		TextView header;
+		TextView map;
+		Button follow;
+	}
+
+}
--- src/com/lavalampe/appenings/EventListFragment.java
+++ src/com/lavalampe/appenings/EventListFragment.java
@@ -1,12 +1,13 @@
 package com.lavalampe.appenings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.ListFragment;
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class EventListFragment extends ListFragment{
@@ -22,13 +23,19 @@ public class EventListFragment extends ListFragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		
 		View v = inflater.inflate(R.layout.fragment_category_list, container,false);
 		
 		String[] menuItems = getResources().getStringArray(R.array.menu_items);
         ListView cardList = (ListView) v.findViewById(R.id.fragment_category_list);
+        
+        List<Category> categories = new ArrayList<Category>();
+        for (int i = 0; i < menuItems.length; i++) {
+			categories.add(new Category(menuItems[i], 85, 85, new User("jostein", "blabla!", "hnnng", "@"), false));
+		}
 		
-		cardList.setAdapter(new ArrayAdapter<String>(v.getContext(),
-                R.layout.category_list_item, menuItems));
+		cardList.setAdapter(new CategoryListAdapter(v.getContext(),
+                R.layout.category_list_item, categories));
 		return v;
 	}
 	
