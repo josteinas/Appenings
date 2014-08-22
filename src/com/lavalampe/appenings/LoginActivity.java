654c443b46769bd4baaf008b439a1e84360f5687
Josteinas <josteinsolaas@gmail.com>
2014-08-22 14:50:48 +0200

    added functionality to registration screen

--- src/com/lavalampe/appenings/LoginActivity.java
+++ src/com/lavalampe/appenings/LoginActivity.java
@@ -37,7 +37,6 @@ public class LoginActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// setting default screen to login.xml
 		setContentView(R.layout.activity_login);
 
 		registerScreen = (TextView) findViewById(R.id.link_to_register);
@@ -45,28 +44,26 @@ public class LoginActivity extends Activity {
 		usernameInput = (EditText) findViewById(R.id.log_username);
 		passwordInput = (EditText) findViewById(R.id.log_password);
 
-		// Listening to register new account link
 		registerScreen.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
-				// Switching to Register screen
+
 				Intent i = new Intent(getApplicationContext(),
 						RegisterActivity.class);
 				startActivity(i);
 			}
 		});
 
-		// Listening to register new account link
 		loginButton.setOnClickListener(new View.OnClickListener() {
+			
 			public void onClick(View v) {
-				Log.d(TAG, "Login button clicked");
 				doLogin();
 			}
 
 		});
 	}
-	
-	private class OnLoginResponse implements AsyncPostCallback{
+
+	private class OnLoginResponse implements AsyncPostCallback {
 
 		@Override
 		public void onPostComplete(PostResult postResult) {
@@ -75,26 +72,33 @@ public class LoginActivity extends Activity {
 			JSONObject jsonResponse = Utils.httpResponseToJSON(response);
 			boolean foundSession = false;
 			try {
-				if(jsonResponse.getBoolean("success")) {
+				if (jsonResponse.getBoolean("success")) {
 					List<Cookie> cookies = result.cookies;
-					Log.d(TAG, String.format("Found %d cookies", cookies.size()));
+					Log.d(TAG,
+							String.format("Found %d cookies", cookies.size()));
 					for (Cookie cookie : cookies) {
-						Log.d(TAG, "found cookie: " + cookie.toString() + " with name: " + cookie.getName());
-						if(cookie.getName().equalsIgnoreCase("JSessionId")) {
-							SharedPreferences pref = getApplicationContext().getSharedPreferences(LoginActivity.sessionPref, Context.MODE_PRIVATE);
+						Log.d(TAG, "found cookie: " + cookie.toString()
+								+ " with name: " + cookie.getName());
+						if (cookie.getName().equalsIgnoreCase("JSessionId")) {
+							SharedPreferences pref = getApplicationContext()
+									.getSharedPreferences(
+											LoginActivity.sessionPref,
+											Context.MODE_PRIVATE);
 							Editor editor = pref.edit();
 							editor.putString("SessionID", cookie.getValue());
-							editor.putString("username", jsonResponse.getString("username"));
+							editor.putString("username",
+									jsonResponse.getString("username"));
 							editor.commit();
 							foundSession = true;
 						}
-					}				
+					}
 				}
 			} catch (JSONException e) {
 				e.printStackTrace();
 			}
-			if(foundSession){
-				SharedPreferences pref = getApplicationContext().getSharedPreferences("SessionPref", 0);
+			if (foundSession) {
+				SharedPreferences pref = getApplicationContext()
+						.getSharedPreferences("SessionPref", 0);
 				Log.d("sessionID", pref.getString("SessionID", "none"));
 				Log.d("username", pref.getString("username", "none"));
 				Intent i = new Intent(getApplicationContext(),
@@ -102,22 +106,18 @@ public class LoginActivity extends Activity {
 				startActivity(i);
 			}
 		}
-					
-				
-			
-		
-		
+
 	}
 
 	private void doLogin() {
 		String username = usernameInput.getText().toString();
 		String password = passwordInput.getText().toString();
 		String serverUrl = getString(R.string.serverUrl) + "/ValidateUser";
-		
+
 		NameValuePair[] params = new NameValuePair[2];
 		params[0] = new BasicNameValuePair("username", username);
 		params[1] = new BasicNameValuePair("password", password);
-		
+
 		AsyncPost post = new AsyncPost(serverUrl, new OnLoginResponse());
 		post.execute(params);
 	}
--- src/com/lavalampe/appenings/RegisterActivity.java
+++ src/com/lavalampe/appenings/RegisterActivity.java
@@ -1,27 +1,126 @@
 package com.lavalampe.appenings;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
- 
+
+import com.lavalampe.appenings.networking.AsyncPost;
+import com.lavalampe.appenings.networking.AsyncPostCallback;
+import com.lavalampe.appenings.networking.PostResult;
+
 public class RegisterActivity extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Set View to register.xml
-        setContentView(R.layout.activity_register);
- 
-        TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
- 
-        // Listening to Login Screen link
-        loginScreen.setOnClickListener(new View.OnClickListener() {
- 
-            public void onClick(View arg0) {
-                                // Closing registration screen
-                // Switching to Login Screen/closing register screen
-                finish();
-            }
-        });
-    }
+
+	private EditText usernameInput, emailInput, passwordInput,
+			reenterPasswordInput;
+	private Button signUpButton;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// Set View to register.xml
+		setContentView(R.layout.activity_register);
+
+		usernameInput = (EditText) findViewById(R.id.reg_username);
+		emailInput = (EditText) findViewById(R.id.reg_email);
+		passwordInput = (EditText) findViewById(R.id.reg_password);
+		reenterPasswordInput = (EditText) findViewById(R.id.reg_reenter_password);
+		signUpButton = (Button) findViewById(R.id.btnRegister);
+
+		TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
+
+		loginScreen.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View arg0) {
+				// Closing registration screen
+				// Switching to Login Screen/closing register screen
+				finish();
+			}
+		});
+		signUpButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				registerUser();
+			}
+
+			
+
+		});
+	}
+	private void registerUser() {
+		String username = usernameInput.getText().toString();
+		String email = emailInput.getText().toString();
+		String password = passwordInput.getText().toString();
+		String reenteredPassword = reenterPasswordInput.getText().toString();
+		
+		boolean correctPassword = password.equals(reenteredPassword);
+		boolean correctEmail = !email.isEmpty();
+		
+		if(correctEmail && correctPassword) {
+			String serverUrl = getString(R.string.serverUrl) + "/CreateUser";
+			
+			NameValuePair[] params = new NameValuePair[3];
+			params[0] = new BasicNameValuePair("username", username);
+			params[1] = new BasicNameValuePair("password", password);
+			params[2] = new BasicNameValuePair("email", email);
+			
+			AsyncPost post = new AsyncPost(serverUrl, new OnRegisterResponse());
+			post.execute(params);
+		} else {
+			String alertMsg = "Email can not be empty. Passwords does not match.";
+			if(correctEmail) {
+				alertMsg = "Passwords does not match.";
+			}
+			if(correctEmail) {
+				alertMsg = "Email can not be empty.";
+			}
+			showDialog(alertMsg, "Invalid input");
+			
+		}
+	}
+	
+	private void showDialog(String alertMsg, String title) {
+		new AlertDialog.Builder(this).setTitle(title)
+		.setMessage(alertMsg).setNeutralButton("OK", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				
+			}
+		}).setIcon(android.R.drawable.ic_dialog_alert).show();
+	}
+		
+	private class OnRegisterResponse implements AsyncPostCallback {
+
+		@Override
+		public void onPostComplete(PostResult postResult) {
+			HttpResponse response = postResult.response;
+			JSONObject json = Utils.httpResponseToJSON(response);
+			try {
+				if(!json.getBoolean("success")) {
+					showDialog("We have a problem on the server.", "Sorry!");
+				} else if(!json.getBoolean("registration_succeeded")) {
+					showDialog("The username is already in use. Please choose another one.", "Password in use");
+				}
+				else {
+					finish();
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
 }
